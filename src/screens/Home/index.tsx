d77b96a320c1647ab489ca9fdf6ca103e61aec7c
Italo Fernandes <italojafernandes@hotmail.com>
2024-07-18 15:35:44 -0300

    🔨 Definindo a estrutura base do projeto, inserindo e estilizando TextInput

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, TextInput, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      {/* <StatusBar style="auto" /> */}
+
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Quinta, 18 de Julho de 2024</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  );
+}
