b1b3154d3fb5c02b6bf3e090d0dd07abd6eefc20
Italo Fernandes <italojafernandes@hotmail.com>
2024-08-18 22:46:20 -0300

    🔨 Trabalhando com método map e ScrollView

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
 
 export default function Home() {
+  const participants = [
+    "Participante 1",
+    "Participante 2",
+    "Participante 3",
+    "Participante 4",
+    "Participante 5",
+    "Participante 6",
+    "Participante 7",
+    "Participante 8",
+    "Participante 9",
+    "Participante 10",
+    "Participante 11",
+    "Participante 12",
+  ];
   const handleParticipantAdd = () => {
     console.log("Você clicou no botão adicionar");
   };
 
-  const handleParticipantRemove = () => {
+  const handleParticipantRemove = (name: string) => {
     console.log("Você clicou em remover participante");
   };
 
   return (
     <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Quinta, 18 de Julho de 2024</Text>
@@ -31,8 +48,16 @@ export default function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
-      <Participant name="Italo" onRemove={handleParticipantRemove} />
-      <Participant name="Diego" onRemove={handleParticipantRemove} />
+
+      <ScrollView>
+        {participants.map((participante) => (
+          <Participant
+            key={participante}
+            name={participante}
+            onRemove={() => handleParticipantRemove("")}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
